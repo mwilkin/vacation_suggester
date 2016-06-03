a3ef6641d1796443cfb9f8373d7cd3008007d111
Matthew Wilkin <mwilkin22@gmail.com>
2016-06-03 11:33:52 -0700

    debugged scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -8,29 +8,27 @@
 $(document).ready(function() {
  $("#quiz").submit(function(event) {
    event.preventDefault();
-   var weather = $("input:radio[name=weather]:checked").val();
-   var location = $("input:radio[name=location]:checked").val();
-   var city = $("input:radio[name=city]:checked").val();
-   var active = $("input:radio[name=active]:checked").val();
-   var cheap = $("input:radio[name=cheap]:checked").val();
-
-   if () {
+   var weather = $("submit:radio[name=weather]:checked").val();
+   var location = $("submit:radio[name=location]:checked").val();
+   var city = $("submit:radio[name=city]:checked").val();
+   var active = $("submit:radio[name=active]:checked").val();
+   var cheap = $("submit:radio[name=cheap]:checked").val();
+   alert("I am here");
+   if (weather) {
+// stuck here. trying to sort out how to build the conditional questions
 
    $(".quiz").hide();
    $(".result").show();
 
- } else {
-   alert("Please answer all of the questions before submitting.");
- }
- });
- $(".result button").click(function(event) {
-   event.preventDefault();
-   $(".result").hide();
-   $(".quiz").show();
- });
- });
+    } else {
+      alert("Please answer all of the questions before submitting.");
+    };
 
+    $(".result button").click(function(event) {
+      event.preventDefault();
+      $(".result").hide();
+      $(".quiz").show();
+    });
 
  });
-
 });
