6820e76b241806d201b3741cf577032adee8c6d6
Matthew Wilkin <mwilkin22@gmail.com>
2016-06-03 10:33:13 -0700

    modified stylesn and started the scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,26 @@
+//Business logic:
 
-var weather = $("input:radio[name=weather]:checked").val();
+
+
+
+//User Interface logic:
+
+$(document).ready(function() {
+ $("#quiz").submit(function(event) {
+   event.preventDefault();
+   var weather = $("input:radio[name=weather]:checked").val();
+   var location = $("input:radio[name=location]:checked").val();
+   var city = $("input:radio[name=city]:checked").val();
+   var active = $("input:radio[name=active]:checked").val();
+   var cheap = $("input:radio[name=cheap]:checked").val();
+
+   if () {
+
+   $(".quiz").hide();
+   $(".result").show();
+
+ } else {
+   alert("Please answer all of the questions before submitting.")
+ }
+ });
+});
