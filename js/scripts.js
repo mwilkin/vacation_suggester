911bdbaa43d138cf1ce70ac5fb9f62152f05656d
Matt Wilkin <mwilkin22@gmail.com>
2016-06-07 23:57:41 -0700

    revised jQuery in an attempt to establish form functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,60 @@
-$(document).ready(function() {
- $("form#submit").submit(function(event) {
+$(function() {
+ $("form#quiz").submit(function(event) {
    event.preventDefault();
 
+   var name = $("input#name").val();
+   var date = $("input#date").val();
    var weather = $("submit:radio[name=weather]:checked").val();
    var location = $("submit:radio[name=location]:checked").val();
    var city = $("submit:radio[name=city]:checked").val();
    var active = $("submit:radio[name=active]:checked").val();
-   var cheap = $("submit:radio[name=cheap]:checked").val();
+   var cost = $("submit:radio[name=cost]:checked").val();
    var total1 = 0;
    var total2 = 0;
    var total3 = 0;
 
 // question 1
-   if (weather === "warm") {
+   if (weather === "b") {
      total1 += 1;
-   } else if (weather === "cold") {
+   } else if (weather === "a") {
      total2 += 1;
-   } else (weather === "no preference"){
+   } else if (weather === "c"){
      total3 += 1;
    }
 
 // question 2
-   if (location === "remote") {
+   if (location === "b") {
       total1 += 1;
-    } else if (location === "near") {
+    } else if (location === "c") {
       total2 += 1;
-    } else (location === "no preference") {
+    } else if (location === "a") {
       total3 += 1;
     }
 
 // question 3
-   if (city === "city") {
+   if (city === "c") {
       total1 += 1;
-    } else if (city === "country") {
+    } else if (city === "a") {
       total2 += 1;
-    } else (city === "no preference") {
+    } else if (city === "b") {
       total3 += 1;
     }
 
 // question 4
-    if (active === "active") {
+    if (active === "c") {
        total1 += 1;
-     } else if (active === "lazy") {
+     } else if (active === "b") {
        total2 += 1;
-     } else (active === "no preference") {
+     } else if (active === "a") {
        total3 += 1;
      }
 
 // question 5
-   if (cost === "expensive") {
+   if (cost === "a") {
       total1 += 1;
-    } else if (cost === "cheap") {
+    } else if (cost === "c") {
       total2 += 1;
-    } else (cost === "no preference") {
+    } else if (cost === "b") {
       total3 += 1;
     }
 
@@ -60,18 +62,19 @@ $(document).ready(function() {
       $("#switzerland").show();
       $("#belize").hide();
       $("#rockaway").hide();
-    } if else (total2 > total1 && total2 > total3) {
-      $("#switzerland").hide();
+    } else if (total2 > total1 && total2 > total3) {
       $("#belize").show();
+      $("#switzerland").hide();
       $("#rockaway").hide();
     } else {
+      $("#rockaway").show();
       $("#switzerland").hide();
       $("#belize").hide();
-      $("#rockaway").show();
+
     }
 
-    $(#name).text(name);
-    $(#date).text(date);
+    // return text(name);
+    // return text(date);
 
  });
 });
